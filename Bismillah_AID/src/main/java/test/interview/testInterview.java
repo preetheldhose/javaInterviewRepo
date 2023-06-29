be66b87646542762fa462e5edfcc881962b9914d
preeth.eldhose@preeth.net <preeth.eldhose@preeth.net>
2023-06-28 17:45:11 -0700

    Latest final addtl

--- Bismillah_AID/src/main/java/test/interview/testInterview.java
+++ Bismillah_AID/src/main/java/test/interview/testInterview.java
@@ -656,9 +656,16 @@ public class testInterview {
 		char[] ch = str.toCharArray();
 		int lenCh = ch.length;
 		logger("Value of lenCh is as follows : ", Integer.toString(lenCh));
+		
+		//HelloWorld
 	
 		for(int left = 0; left < lenCh/2; left++) {
 			Character nCh = new Character(ch[left]);
+			logger("Left value : ", nCh.toString());
+		}
+
+		for(int right = lenCh-1; right >= lenCh/2; right--) {
+			Character nCh = new Character(ch[right]);
 			logger("Left value : ", nCh.toString());
 		}
 
