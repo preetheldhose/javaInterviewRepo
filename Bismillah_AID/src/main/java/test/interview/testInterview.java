1d38b2c60f8ff0d54909816a922de423c548e7fd
preeth.eldhose@preeth.net <preeth.eldhose@preeth.net>
2023-06-29 20:05:48 -0700

    Latest final addtl

--- Bismillah_AID/src/main/java/test/interview/testInterview.java
+++ Bismillah_AID/src/main/java/test/interview/testInterview.java
@@ -897,21 +897,21 @@ public class testInterview {
 		StringBuffer strBuf = new StringBuffer();
 		for(String str : strComPrefix) {
 			char[] ch = str.toCharArray();
-		//	for(int i =0; i < ch.length; i++) {
-		//		System.out.println("i- values : " + ch[i]);
+			for(int i =0; i < ch.length; i++) {
+				//System.out.println("i- values : " + ch[i]);
 			for(int j = ch.length-1; j > 0; j--) {
-				System.out.println("j- values : " + ch[j]);
-//					if(ch[i] == ch[j]) {
-//						count++;
-//						if(count >= previousCount) {
-//							strBuf.append(ch[i]);
-//						}
-//					}
+				//System.out.println("j- values : " + ch[j]);
+					if(ch[i] == ch[j]) {
+						count++;
+						if(count >= previousCount) {
+							strBuf.append(ch[i]);
+						}
+					}
 			}
 			}
-//		}
+		}
 		
-		//System.out.println("Longest common prefix is as follows : " + strBuf);
+		System.out.println("Longest common prefix is as follows : " + strBuf);
 
 	}
 
