39083fb8138ea457d8a3dd66553a834d2de2facb
preeth.eldhose@preeth.net <preeth.eldhose@preeth.net>
2023-07-02 12:13:23 -0700

    This is pre-final

--- appiumAID/src/test/java/appiumGID/appiumAID/AppiumBasics.java
+++ appiumAID/src/test/java/appiumGID/appiumAID/AppiumBasics.java
@@ -3,42 +3,28 @@ package appiumGID.appiumAID;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 
-public class AppiumBasics {
-	
+public class AppiumBasics extends BaseTest{
+
 	@Test
-	public void appiumTest() throws MalformedURLException, InterruptedException{
-		
-		//start the server which is at this path = // /usr/local/lib/node_modules/appium/build/lib/main.js
-		AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(new File("//usr//local//lib//node_modules//appium//build//lib//main.js"))
-				.withIPAddress("127.0.0.1").usingPort(4723).build();
-		service.start();
-		
-		
-		
-		//AndroidDriver Object:
-		//Appium code is written and sent to the Appium Server - which in then talks to UIAutomator2,
-		//which in-turn talks to the simulator or the app in the Android device.
-		//Capabilities = 
-		
-		
-		UiAutomator2Options options = new UiAutomator2Options();
-		options.setDeviceName("RahulEmulator");
-		options.setApp("//Users//preetheldhose//eclipse-workspace//appiumAID//src//test//java//resouces//APKFiles.zip");
-		
-		
-		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
-		Thread.sleep(25000);
-		driver.quit();
-		
-		Thread.sleep(25000);
-		//stop the server
-		service.stop();
+	public void appiumTest() throws MalformedURLException, InterruptedException {
+		//ConfigureAppium();
+		Thread.sleep(8000);
+		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+		Thread.sleep(8000);
+		//tearDown();
+
 	}
 
 }
---
+++ appiumAID/src/test/java/appiumGID/appiumAID/BaseTest.java
@@ -0,0 +1,47 @@
+package appiumGID.appiumAID;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
+
+public class BaseTest {
+	
+	public AndroidDriver driver;
+	
+	@BeforeClass
+	public void ConfigureAppium() throws InterruptedException, MalformedURLException {
+		
+		/*
+		AndroidDriver Object: 
+		1. Appium code is written and sent to the Appium Server - which in then talks to
+		2. UIAutomator2 which in-turn talks to the simulator or the app in the Android device via Capabilities
+		*/
+		
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setDeviceName("RahulEmulator");
+		options.setApp("//Users//preetheldhose//eclipse-workspace//appiumAID//src//test//java//resources//APKFiles.zip");
+
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+		
+	}
+	
+	@AfterClass
+	public void tearDown() throws InterruptedException {	
+		driver.quit();
+	}
+
+
+}
