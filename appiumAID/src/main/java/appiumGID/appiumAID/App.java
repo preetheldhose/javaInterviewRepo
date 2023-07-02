c21cdbfc83509bc0badecf1db70aec927d99da70
preeth.eldhose@preeth.net <preeth.eldhose@preeth.net>
2023-07-01 21:20:52 -0700

    appium project

---
+++ appiumAID/src/main/java/appiumGID/appiumAID/App.java
@@ -0,0 +1,13 @@
+package appiumGID.appiumAID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
